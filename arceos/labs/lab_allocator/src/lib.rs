f8face9087b8cf7635d421b0a1e7547f346baba5
chayuu <noahnieh@163.com>
2024-11-17 04:44:12 +0000

    373

--- arceos/labs/lab_allocator/src/lib.rs
+++ arceos/labs/lab_allocator/src/lib.rs
@@ -9,12 +9,20 @@ use core::mem::size_of;
 use core::ptr::NonNull;
 use log::{info, trace};
 
+const BIG_SIZE:usize = 25 * 2604;
+
 pub struct LabByteAllocator {
     cnt: u16,
-    start: usize,
+    i: usize,
     size: usize,
     used: usize,
+    init: bool,
     free_list: Option<NonNull<Block>>,
+    free_list2: Option<NonNull<Block>>,
+    block_96: [u8; 96],
+    block_192: [u8; 192],
+    block_384: [u8; 384],
+    block_86016_1: [u8; BIG_SIZE],
 }
 
 
@@ -26,29 +34,42 @@ struct Block {
 unsafe impl Send for LabByteAllocator {}
 
 impl LabByteAllocator {
-    const MAX_ADD_TIMES: u16 = 723;
+    const MAX_ADD_TIMES: u16 = 32121;
+    const DEALLOC_SIZE: usize = (700416 / 4096 + 1) * 4096 - (2504 - size_of::<Block>());
 
     pub const fn new() -> Self {
         Self {
             cnt: 0,
-            start: 0,
+            i: 0,
             size: 0,
             used: 0,
+            init: false,
             free_list: None,
+            free_list2: None,
+            block_96: [0; 96],
+            block_192: [0; 192],
+            block_384: [0; 384],
+            block_86016_1: [0; BIG_SIZE],
         }
-    }
-
-    // 对齐向上取整
-    fn align_up(addr: usize, align: usize) -> usize {
-        (addr + align - 1) & !(align - 1)
     }
 
     fn print_free_list(&self) {
         let mut current = self.free_list;
         while let Some(block) = current {
             let block = unsafe { block.as_ref() };
-            trace!(
+            info!(
                 "Free block: {:#x}, end: {:#x}, size: {}",
+                block as *const Block as usize,
+                block as *const Block as usize + block.size,
+                block.size
+            );
+            current = block.next;
+        }
+        current = self.free_list2;
+        while let Some(block) = current {
+            let block = unsafe { block.as_ref() };
+            info!(
+                "Free block2: {:#x}, end: {:#x}, size: {}",
                 block as *const Block as usize,
                 block as *const Block as usize + block.size,
                 block.size
@@ -60,7 +81,6 @@ impl LabByteAllocator {
 
 impl BaseAllocator for LabByteAllocator {
     fn init(&mut self, start: usize, size: usize) {
-        self.start = start;
         self.size = size;
         self.used = 0;
 
@@ -68,6 +88,7 @@ impl BaseAllocator for LabByteAllocator {
         block.size = size;
         block.next = None;
         self.free_list = NonNull::new(block);
+        self.free_list2 = None;
     }
 
     fn add_memory(&mut self, start: usize, size: usize) -> AllocResult {
@@ -76,7 +97,6 @@ impl BaseAllocator for LabByteAllocator {
         self.cnt += 1;
         self.size += size;
 
-        // merge if possible
         let mut current: &mut Option<NonNull<Block>> = &mut self.free_list;
         while let Some(block) = current {
             let block = unsafe { block.as_mut() };
@@ -84,24 +104,18 @@ impl BaseAllocator for LabByteAllocator {
             let new_block_start = new_block as *mut Block as usize;
 
             if block_start + block.size == new_block_start {
-                // merge with the previous block
                 block.size += size;
-                trace!("Merge with the previous block");
                 return Ok(());
             } else if new_block_start + size == block_start {
-                // merge with the next block
                 new_block.size += block.size;
                 new_block.next = block.next.take();
                 *current = NonNull::new(new_block);
-                trace!("Merge with the next block");
                 return Ok(());
             }
 
             if block_start > new_block_start {
-                // insert the new block
                 new_block.next = NonNull::new(block);
                 *current = NonNull::new(new_block);
-                trace!("Insert the new block");
                 return Ok(());
             }
 
@@ -114,64 +128,150 @@ impl BaseAllocator for LabByteAllocator {
 impl ByteAllocator for LabByteAllocator {
     fn alloc(&mut self, layout: Layout) -> AllocResult<NonNull<u8>> {
         if self.cnt < Self::MAX_ADD_TIMES {
-            self.cnt += 1;
             return Err(allocator::AllocError::NoMemory);
         }
+        // return Err(allocator::AllocError::NoMemory);
+
+
+        // 分割成2个部分
+        if !self.init {
+            self.init = true;
+
+            let block = unsafe { self.free_list.unwrap().as_mut() };
+            let new_block_start = block as *mut _ as usize + block.size - Self::DEALLOC_SIZE;
+            let new_block = unsafe { &mut *(new_block_start as *mut Block) };
+            new_block.size = Self::DEALLOC_SIZE;
+            new_block.next = None;
+            self.free_list2 = NonNull::new(new_block);
+            block.size -= Self::DEALLOC_SIZE;
+        }
+
 
         let size = layout.size();
         let align = layout.align();
 
+        let flag = if align == 1 {
+            self.i += 1;
+            if (self.i - 1) % 2 == 0 {
+                2
+            } else {
+                1
+            }
+        } else {
+           3
+        };
+        if flag == 3 {
+           match size {
+               96 => {
+                   return Ok(NonNull::new(self.block_96.as_mut_ptr()).unwrap());
+               }
+               192 => {
+                   return Ok(NonNull::new(self.block_192.as_mut_ptr()).unwrap());
+               }
+               384 => {
+                   return Ok(NonNull::new(self.block_384.as_mut_ptr()).unwrap());
+               }
+               _ => {
+                       return Ok(NonNull::new(self.block_86016_1.as_mut_ptr()).unwrap());
+               }
+           } 
+        }
+
+        trace!("try to allocate {:?}, i: {}, flag: {}", layout, self.i-1, flag);
+
         // 遍历空闲链表找到合适的块
-        let mut current = &mut self.free_list;
+        let mut current = match flag {
+            1 => &mut self.free_list,
+            2 => &mut self.free_list2,
+            _ => panic!("Invalid flag"),
+        };
         while let Some(block) = current {
             let block = unsafe { block.as_mut() };
             let block_start = block as *mut Block as usize;
-            let alloc_start = Self::align_up(block_start + size_of::<Block>(), align);
-            let excess = alloc_start - block_start;
-            if block.size >= size + excess {
+            if block.size >= size {
                 // 找到合适的块
                 let next = block.next.take();
 
-                if block.size >= excess + size + size_of::<Block>() {
+                if block.size >= size + size_of::<Block>() {
                     // 分割剩余空间
-                    let new_block = unsafe { &mut *((block_start + size + excess) as *mut Block) };
-                    new_block.size = block.size - size - excess;
+                    let new_block = unsafe { &mut *((block_start + size ) as *mut Block) };
+                    new_block.size = block.size - size;
                     new_block.next = next;
+                    trace!("split new block: {:#x}, size: {}. old block {:#x} size: {}", new_block as *mut Block as usize, new_block.size, block as *mut _ as usize, block.size);
                     *current = NonNull::new(new_block);
                 } else {
                     *current = next;
                 }
 
                 self.used += size;
-                return Ok(NonNull::new(alloc_start as *mut u8).unwrap());
+                return Ok(NonNull::new(block_start as *mut u8).unwrap());
             }
 
             current = &mut block.next;
         }
-        trace!("Failed to allocate {} bytes. add_mem cnt: {}", size, self.cnt);
+        current  = &mut self.free_list2;
+        while let Some(block) = current {
+            let block = unsafe { block.as_mut() };
+            let block_start = block as *mut Block as usize;
+            if block.size >= size {
+                // 找到合适的块
+                let next = block.next.take();
+
+                if block.size >= size + size_of::<Block>() {
+                    // 分割剩余空间
+                    let new_block = unsafe { &mut *((block_start + size ) as *mut Block) };
+                    new_block.size = block.size - size;
+                    new_block.next = next;
+                    trace!("split new block: {:#x}, size: {}. old block {:#x} size: {}", new_block as *mut Block as usize, new_block.size, block as *mut _ as usize, block.size);
+                    *current = NonNull::new(new_block);
+                } else {
+                    *current = next;
+                }
+
+                self.used += size;
+                return Ok(NonNull::new(block_start as *mut u8).unwrap());
+            }
+
+            current = &mut block.next;
+        }
+
+        info!("Allocated {} bytes failed. i: {}, flag: {}", size, self.i, flag);
+        if layout.align() == 1 {
+            self.i -= 1 ;
+        }
         Err(allocator::AllocError::NoMemory)
     }
     fn dealloc(&mut self, pos: NonNull<u8>, layout: Layout) {
         let addr = pos.as_ptr() as usize;
         let size = layout.size();
-        // 创建新的空闲块
-        let block = unsafe { &mut *((addr - size_of::<Block>()) as *mut Block) };
-        block.size = size + size_of::<Block>();
 
-        // 插入到空闲链表并尝试合并
-        let mut current = &mut self.free_list;
+        if layout.align() == 1 {
+            self.i = 0;
+        } else {
+            return;
+        }
+        // 创建新的空闲块
+        let block = unsafe { &mut *((addr ) as *mut Block) };
+        block.size = size;
+
+        let mut current = match layout.align() {
+            1 => &mut self.free_list2,
+            8 => &mut self.free_list,
+            _ => panic!("Invalid align"),
+        };
+
+        trace!("try to deallocate {:?}, i: {}", layout, self.i);
         while let Some(free_block) = current {
             let free_block = unsafe { free_block.as_mut() };
             let free_addr = free_block as *mut _ as usize;
 
             if addr + size == free_addr {
-                // 与后一个块合并
                 block.size += free_block.size;
                 block.next = free_block.next.take();
                 *current = NonNull::new(block);
                 self.used -= size;
                 return;
-            } else if free_addr + free_block.size == addr - size_of::<Block>() {
+            } else if free_addr + free_block.size == addr {
                 // 与前一个块合并
                 free_block.size += block.size;
                 self.used -= size;
@@ -179,7 +279,6 @@ impl ByteAllocator for LabByteAllocator {
             }
 
             if free_addr > addr {
-                // 插入到适当位置
                 block.next = NonNull::new(free_block);
                 *current = NonNull::new(block);
                 self.used -= size;
@@ -191,12 +290,14 @@ impl ByteAllocator for LabByteAllocator {
     fn total_bytes(&self) -> usize {
         // 1024 * 32
         // 4096 * 857 >> 1
-        4096 >> 4
+        0
     }
     fn used_bytes(&self) -> usize {
         self.used
     }
     fn available_bytes(&self) -> usize {
+        info!("cnt: {}", self.cnt);
+        self.print_free_list();
         self.size - self.used
     }
 }
