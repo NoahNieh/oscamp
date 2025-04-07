9ed8efc203014f75023a9be5403544f472cc16b7
NoahNieh <noahnieh@163.com>
2025-04-07 11:17:22 +0800

    env script

---
+++ env.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+
+sudo apt update && sudo apt install -y make cmake build-essential
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+
+# rustup
+type cargo > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    . "$HOME/.cargo/env"
+fi
+
+# rust utils
+cargo install cargo-binutils
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+# qemu
+sudo apt install -y qemu-system
+
+# sanity check
+
+cat > env_info << EOF
+-----info-----------
+qemu: $(qemu-system-riscv64 --version | head -n1)
+cargo: $(cargo --version)
+EOF
+
+cat env_info 
+
+
+
+
